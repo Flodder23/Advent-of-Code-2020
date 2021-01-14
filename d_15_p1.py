090f26f8a318d4c530b5ba8ca7faa870ae5220d1
Joe Gibson <joe.gibby@hotmail.co.uk>
2021-01-14 10:31:06 +0000

    Day 15, Part 1

---
+++ d_15_p1.py
@@ -0,0 +1,13 @@
+from d_15_parse import input_parse
+
+numbers = input_parse()
+last_said = {numbers[i]: i for i in range(len(numbers))}
+next_number = 0
+
+for i in range(len(numbers), 2020):
+	if not next_number in last_said:
+		last_said[next_number] = i
+	last_number = next_number
+	next_number = i - last_said[last_number]
+	last_said[last_number] = i
+print(last_number)
---
+++ d_15_parse.py
@@ -0,0 +1,5 @@
+def input_parse(filename="d_15_input.txt"):
+	return list(map(int, open(filename, "r").read().split(",")))
+
+if __name__ == "__main__":
+	print(input_parse())
