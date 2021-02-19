1af56c7f631ca44de732def97482258d45c3ed1f
Joe Gibson <joe.gibby@hotmail.co.uk>
2021-02-19 01:19:01 +0000

    Day 22, Part 2 - Optimised, runs in <1.4s

--- d_22_p2.py
+++ d_22_p2.py
@@ -1,58 +1,36 @@
+### Optimised using ideas from here: 
+### 	https://github.com/hltk/adventofcode/blob/main/2020/22.py,
+### 	https://github.com/viliampucik/adventofcode/blob/master/2020/22.py
+
 from d_22_parse import input_parse
 from collections import deque
-from time import time
+import itertools
 
-def compare_deque(d1, d2):
-	return len(d1) == len(d2) and all([d1[i] == d2[i] for i in range(len(d1))])
-
-def play(player1, player2, history, game=1):
-#	print(f"\n=== Game {game} ===")
-	r = 0
-	winner = None
-	while winner is None:
-		r += 1
-#		print(f"\n-- Round {r} (Game {game}) --")
-#		print(f"Player 1's deck: {', '.join([str(n) for n in player1])}")
-#		print(f"Player 2's deck: {', '.join([str(n) for n in player2])}")
-		if any([compare_deque(player1, d1) and compare_deque(player2, d2) for [d1, d2] in history]):
-#			print("déjà vu")
-			return player1, 1
-		history.append([player1.copy(), player2.copy()])
+def play(player1, player2, return_deck=False):
+	history = set()
+	while True:
+		if (state := (tuple(player1), tuple(player2))) in history:
+			return player1 if return_deck else 1
+		history.add(state)
 		p1 = player1.popleft()
 		p2 = player2.popleft()
-#		print(f"Player 1 plays: {p1}")
-#		print(f"Player 2 plays: {p2}")
 		if p1 > len(player1) or p2 > len(player2):
-			if p1 > p2:
-				player1.extend((p1, p2))
-#				print(f"Player 1 wins game {game}, round {r}!")
-			else:
-				player2.extend((p2, p1))
-#				print(f"Player 2 wins game {game}, round {r}!")
+			winner = 1 if p1 > p2 else 2
 		else:
-#			print("Playing a sub-game to determine the winner...")
-			p1c, p2c = player1.copy(), player2.copy()
-			_, no = play(deque([p1c.popleft() for _ in range(p1)]), deque([p2c.popleft() for _ in range(p2)]), history.copy())
-#			print(f"\n...anyway, back to game {game}.")
-			if no == 1:
-				player1.extend((p1, p2))
-#				print(f"Player 1 wins game {game}, round {r}!")
-			else:
-				player2.extend((p2, p1))
-#				print(f"Player 2 wins game {game}, round {r}!")
+			winner = play(
+				deque(itertools.islice(player1, 0, p1)),
+				deque(itertools.islice(player2, 0, p2))
+			)
+		if winner == 1:
+			player1.extend((p1, p2))
+		else:
+			player2.extend((p2, p1))
 		if len(player1) == 0:
-			winner = player2
-			no = 2
+			return player2 if return_deck else 2
 		elif len(player2) == 0:
-			winner = player1
-			no = 1
-	return winner, no
+			return player1 if return_deck else 1
 
 
-player1, player2 = input_parse()#"d_22_input_example.txt")
-
-t1 = time()
-winner, _ = play(player1, player2, [])
-print(time() - t1)
-
+player1, player2 = input_parse()
+winner = play(player1, player2, return_deck=True)
 print(sum([(i+1) * winner.pop() for i in range(len(winner))]))
